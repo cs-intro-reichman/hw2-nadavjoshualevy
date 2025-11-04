1c97899f5b376d43e101f2539f6891037e96d345
Nadav Joshua Levy <nadavjoshualevy@Nadavs-MacBook-Air.local>
2025-11-04 09:38:26 +0200

    1.0

--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,45 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+       
+        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        int minutesToAdd = Integer.parseInt(args[1]);
+       
+
+        int totalMinutes = (hours*60) + minutes + minutesToAdd; 
+        int totalHours = totalMinutes/60;
+        int newHours = totalHours%24;
+        int newMinutes = totalMinutes- (totalHours*60);
+
+    
+
+        if (hours>12)
+        { hours = 24-hours;}
+          
+
+
+        String time = "";
+
+      if (newHours<10 && newMinutes>10) {time = "0" + newHours + ":" + newMinutes;}
+      if (newHours<10 && newMinutes<10) {time = "0" + newHours + ":0" + newMinutes;}
+      if (newHours>10 && newMinutes<10) {time =  + newHours + ":0" + newMinutes;}
+      if (newHours>10 && newMinutes>10) {time =  + newHours + ":" + newMinutes;}
+      if (newHours==10 && newMinutes==10) {time =  + newHours + ":" + newMinutes;}
+       if (newHours==10 && newMinutes>10) {time =  + newHours + ":" + newMinutes;}
+       if (newHours==10 && newMinutes<10) {time =  + newHours + ":0" + newMinutes;}
+       if (newHours>10 && newMinutes==10) {time =  + newHours + ":" + newMinutes;}
+       if (newHours<10 && newMinutes==10) {time = "0" + newHours + ":" + newMinutes;}
+
+
+
+
+       System.out.println( time );
+    
+    
+    
+
+    
+  
+		
     }
 }

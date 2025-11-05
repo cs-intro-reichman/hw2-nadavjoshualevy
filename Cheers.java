ca5a822f1a0e963cd222dc5aeb217a49b1d0fa9d
Nadav Joshua Levy <nadavjoshualevy@Nadavs-MacBook-Air.local>
2025-11-05 20:15:44 +0200

    3.3

--- Cheers.java
+++ Cheers.java
@@ -4,9 +4,10 @@ public class Cheers {
 	    String word = args[0];
         int amount = Integer.parseInt(args[1]);
                 String an = "AEFHILMNORSXaefhilmnorsx";
+                word= word.toUpperCase();
             for (int i = 0 ; i<word.length() ; i++){
                 char c = word.charAt(i);
-                if (an.indexOf(c)==-1) { System.out.println("Give me a  " + word.charAt(i) + ": " + word.charAt(i) + "!");}
+                if (an.indexOf(c)==-1) { System.out.println("Give me a  " + (word.charAt(i)) + ": " + word.charAt(i) + "!");}
                   else { System.out.println("Give me an " + word.charAt(i) + ": " + word.charAt(i) + "!");       
         
             }
@@ -14,7 +15,7 @@ public class Cheers {
                 System.out.println("What does that spell?");
 
                 for (int i = 0; i<amount; i++){
-                        System.out.println(word+ "!!!");
+                        System.out.println(word + "!!!");
                 }
 }
 }

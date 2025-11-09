bfd8afff6e9d8c41e4fbee917e3503e717a2f035
Nadav Joshua Levy <nadavjoshualevy@Nadavs-MacBook-Air.local>
2025-11-09 17:17:15 +0200

    4.0

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,22 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	  
+		int terms = Integer.parseInt(args[0]);
+		double count = 0.0;
+		double pie = Math.PI;
+		for (int i = 0; i<terms; i++){
+			double neo = 2 * i + 1;
+			double neoq = 1.0/neo;
+			if (i%2 == 0){count = count + neoq;}
+			else{count = count - neoq;}
+		}
+		double piApprox = count * 4;
+
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated: " + piApprox);
+
 	}
+
+	
 }

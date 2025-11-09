42fa68acb402c2111e606edfc2c70db2a39d62db
Nadav Joshua Levy <nadavjoshualevy@Nadavs-MacBook-Air.local>
2025-11-09 17:20:21 +0200

    4.1

--- CalcPi.java
+++ CalcPi.java
@@ -14,7 +14,7 @@ public class CalcPi {
 		double piApprox = count * 4;
 
 		System.out.println("pi according to Java: " + Math.PI);
-		System.out.println("pi, approximated: " + piApprox);
+		System.out.println("pi, approximated:     " + piApprox);
 
 	}
 
